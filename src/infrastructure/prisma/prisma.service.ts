fa1c0f66ec114b7fc2b988216e6baae4c04524d0
david-cam <32599516+david-cam@users.noreply.github.com>
2023-01-29 18:37:57 -0500

    adding prisma module/service

---
+++ src/infrastructure/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  //Why:
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+
+  async enableShutdownHooks(app: INestApplication): Promise<void> {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}

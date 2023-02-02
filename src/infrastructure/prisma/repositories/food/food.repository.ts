d813bf679fb3503bb343cb4ec6ea333888789e89
davidcamwt <david.campbell@willowtreeapps.com>
2023-02-02 14:21:51 -0500

    editing docker compose, there was no return in repository function

--- src/infrastructure/prisma/repositories/food/food.repository.ts
+++ src/infrastructure/prisma/repositories/food/food.repository.ts
@@ -1,38 +1,43 @@
-import { Result } from "@badrap/result";
-import { Injectable, Logger } from "@nestjs/common";
-import { Food } from "src/core/food/food.entity";
-import { FoodCreateRequest, FoodRepository, FoodRepositoryErrorResponse, InvalidState, UnknownError } from "src/core/food/food.repository";
-import { PrismaService } from "../../prisma.service";
-import { mapDbEntityToDomainEntity } from "./mapper";
+import { Result } from '@badrap/result';
+import { Injectable, Logger } from '@nestjs/common';
+import { Food } from 'src/core/food/food.entity';
+import {
+  FoodCreateRequest,
+  FoodRepository,
+  FoodRepositoryErrorResponse,
+  InvalidState,
+  UnknownError,
+} from 'src/core/food/food.repository';
+import { PrismaService } from '../../prisma.service';
+import { mapDbEntityToDomainEntity } from './mapper';
 
 @Injectable()
 export class FoodRepositoryImpl implements FoodRepository {
+  //Why:
+  private readonly logger = new Logger(FoodRepository.name);
 
-    //Why:
-    private readonly logger = new Logger(FoodRepository.name)
+  constructor(private readonly prisma: PrismaService) {}
 
-    constructor(private readonly prisma: PrismaService) {}
+  async create(
+    req: FoodCreateRequest,
+  ): Promise<Result<Food, FoodRepositoryErrorResponse>> {
+    try {
+      //TODO: Create works, but there's still an error
+      const entity = await this.prisma.food.create({
+        data: req,
+      });
 
-    async create(req: FoodCreateRequest): Promise<Result<Food, FoodRepositoryErrorResponse>> {
-        try {
-            //TODO: Create works, but there's still an error
-            const entity = await this.prisma.food.create({
-                data:req
-            });
+      if (entity) {
+        return Result.ok(mapDbEntityToDomainEntity(entity));
+      }
 
-            if(entity) {
-                Result.ok(mapDbEntityToDomainEntity(entity))
-            }
-
-            return Result.err(new InvalidState());
-        }
-        catch (e) {
-            this.logger.error(e);
-            return Result.err(new UnknownError())
-        }
+      return Result.err(new InvalidState());
+    } catch (e) {
+      this.logger.error(e);
+      return Result.err(new UnknownError());
     }
-    getAllFoods(): Promise<Result<Food, FoodRepositoryErrorResponse>> {
-        throw new Error("Method not implemented.");
-    }
-
+  }
+  getAllFoods(): Promise<Result<Food, FoodRepositoryErrorResponse>> {
+    throw new Error('Method not implemented.');
+  }
 }

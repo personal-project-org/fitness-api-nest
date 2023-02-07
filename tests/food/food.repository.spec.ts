1f4594fa8dc82ad6a3270d3a21bc180da37344e2
David Campbell <102170536+davidcamwt@users.noreply.github.com>
2023-02-07 16:03:54 -0500

    Feature/GitHub workflow (#3)
    * adding PR template and ci.yml
    
    * adding test stuff

--- tests/food/food.repository.spec.ts
+++ tests/food/food.repository.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Food } from 'src/api/food/entities/local-model/food.entity';
 import { FoodApiModule } from 'src/api/food/food-api.module';
-import { FoodRepository } from 'src/api/food/repository/food.repository';
+import {
+  FoodCreateRequest,
+  FoodRepository,
+} from 'src/api/food/repository/food.repository';
 import { PrismaModule } from 'src/infrastructure/prisma/prisma.module';
 import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
 
@@ -19,5 +23,37 @@ describe('FoodRepository', () => {
 
     foodRepository = module.get<FoodRepository>(FoodRepository);
     expect(foodRepository).toBeDefined();
+
+    prisma = module.get<PrismaService>(PrismaService);
+    expect(prisma).toBeDefined();
+  });
+
+  describe('createFood', async () => {
+    it('Should create a new food successfully when correctly called', async () => {
+      const foodCreateRequest: FoodCreateRequest = {
+        name: 'Soup',
+        calories: '100',
+        protein: '3',
+        carbs: '27',
+        fats: '50',
+      };
+      const foodCreateResult = await foodRepository.create(foodCreateRequest);
+      expect(foodCreateResult).toBeTruthy();
+      expect(foodCreateResult.isOk).toBeTruthy();
+
+      const foodFindResult = await prisma.food.findUnique({
+        where: {
+          name: 'Soup',
+        },
+      });
+
+      const createdFood = foodCreateResult.unwrap();
+
+      expect(createdFood.name).toEqual('Soup');
+      expect(createdFood.calories).toEqual('100');
+      expect(createdFood.protein).toEqual('3');
+      expect(createdFood.carbs).toEqual('27');
+      expect(createdFood.fats).toEqual('50');
+    });
   });
 });

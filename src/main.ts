8f8653fa49db81d750e231474b999ce5cef16c3a
davidcamwt <david.campbell@willowtreeapps.com>
2023-03-09 10:27:45 -0500

    modifying scripts, port number and database url

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  // await app.listen(process.env.PORT || 3000);
+  await app.listen(8080);
 }
 
 bootstrap();
